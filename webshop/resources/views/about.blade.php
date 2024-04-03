429d8d7403b5c9e5c102daf4fe0cf331c95a3a40
Nagy János <batyus4563@gmail.com>
2024-04-03 11:12:06 +0200

    about view alap

---
+++ webshop/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rólunk</title>
+
+    
+</head>
+<body>
+
+<h1>A cégről</h1>
+    
+</body>
+</html>
