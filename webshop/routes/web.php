61b98f25054a202ce6ed6bdb335a28fd0ac0a61a
JaniITmer <batyus4563@gmail.com>
2024-04-06 19:19:57 +0200

    Weboldal cím átírása, bejelentkezés ,regisztráció szöveg magyarositas

--- webshop/routes/web.php
+++ webshop/routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     
-    return view('home');
+    return view('welcome');
 });
 
 
 
 
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
